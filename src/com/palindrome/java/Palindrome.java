67f0b97208408d08faa5ce0ca0fe1dcf54d01109
Kate Catlin <kate.catlin@detroitlabs.com>
2014-09-15 15:09:58 -0400

    This is done.

--- src/com/palindrome/java/Palindrome.java
+++ src/com/palindrome/java/Palindrome.java
@@ -3,9 +3,35 @@ package com.palindrome.java;
 /**
  * Created by katecatlin on 9/15/14.
  */
+
+import java.util.Scanner;
+
+
 public class Palindrome {
 
+
     public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+
+        String userWord;
+        String userWordUp;
+        boolean tastesLikeCandy = false;
+
+            System.out.println("Welcome to the palindrome tester!");
+            System.out.println("Enter a word to test: ");
+        userWord = myScanner.nextLine();
+
+        userWord = userWord.toLowerCase();
+        userWord = userWord.replace(" ", "");
+        userWordUp = new StringBuilder(userWord).reverse().toString();
+
+            if (userWord.equals(userWordUp)) {
+                tastesLikeCandy = true;
+                System.out.println("It's a palindrome!");
+            } else
+            {System.out.println("Sorry, that's not a palindrome! Try again!");
+            tastesLikeCandy = false;}
 
     }
 }
